6f5638f1407ec9e0762154de416c21acbd5c9407
Dasha Moyer <dashamoyer@gmail.com>
2020-04-24 20:18:16 -0700

    In-Game menu
    Basic buttons, no functionality, work in progress

---
+++ Assets/Scripts/UI Scripts/pauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+  //  public Button menuBtn;
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    //    GameIsPaused = false;
+
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+     //   GameIsPaused = true;
+    }
+
+}
