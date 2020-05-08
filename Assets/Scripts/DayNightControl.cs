3aa96ef371424d34fa9b624f0a4c5cc9194b9784
smithDamon <35517446+smithDamon@users.noreply.github.com>
2020-05-08 10:50:27 -0700

    Modifications to DayNightControl
    Removed skyboxes and set the time to not advance unless a timeAdvaces is true

--- Assets/Scripts/DayNightControl.cs
+++ Assets/Scripts/DayNightControl.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class DayNightControl : MonoBehaviour
 {
+    public bool timeAdvances;
     public Light sun;
     public Light moon;
-    public Material daySky;
-    public Material nightSky;
     public float secondsInFullDay = 120f;
     [Tooltip("0 is night, 0.5 is mid day")]
     [Range(0, 1)]
@@ -19,14 +18,16 @@ public class DayNightControl : MonoBehaviour
     void Start()
     {
         sunInitialIntensity = sun.intensity;
-        RenderSettings.skybox = daySky;
     }
 
     void Update()
     {
         UpdateSun();
-
-        currentTimeOfDay += (Time.deltaTime / secondsInFullDay);
+        if (timeAdvances == true)
+        { 
+            currentTimeOfDay += (Time.deltaTime / secondsInFullDay);
+        }
+        
 
         if (currentTimeOfDay >= 1)
         {
@@ -48,7 +49,6 @@ public class DayNightControl : MonoBehaviour
             RenderSettings.fogDensity = 0.01f;
             intensityMultiplier = 0;
             moon.intensity = 0.175f;
-            RenderSettings.skybox = nightSky;
         }
         else if (currentTimeOfDay <= 0.25f)
         {
@@ -56,14 +56,12 @@ public class DayNightControl : MonoBehaviour
             RenderSettings.fog = false;
             intensityMultiplier = Mathf.Clamp01((currentTimeOfDay - 0.23f) * (1 / 0.02f));
             moon.intensity = 0f;
-            RenderSettings.skybox = daySky;
         }
         else if (currentTimeOfDay >= 0.73f)
         {
             RenderSettings.fog = false;
             intensityMultiplier = Mathf.Clamp01(1 - ((currentTimeOfDay - 0.73f) * (1 / 0.02f)));
             moon.intensity = 0f;
-            RenderSettings.skybox = daySky;
         }
 
         sun.intensity = sunInitialIntensity * intensityMultiplier;
