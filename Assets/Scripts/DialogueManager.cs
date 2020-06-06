f9214fd4c2573fa37ea9892569bb5a72a35d29ae
smithDamon <35517446+smithDamon@users.noreply.github.com>
2020-06-05 21:49:11 -0700

    added Image spawner, spritePrefab

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class DialogueManager : MonoBehaviour {
+public class DialogueManager : MonoBehaviour
+{
 
     public Text nameText;
     public Text dialogueText;
@@ -17,22 +18,26 @@ public class DialogueManager : MonoBehaviour {
     private Queue<string> sentences;
     //private Queue<string> responses;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         sentences = new Queue<string>();
         //responses = new Queue<string>();
-	}
+    }
 
-	public void StartDialogue(Dialogue dialogue)
+    public void StartDialogue(Dialogue dialogue)
     {
         continueButton.SetActive(true);
+        continueButton.GetComponent<Button>().onClick.RemoveAllListeners();
+        continueButton.GetComponent<Button>().onClick.AddListener(() => { DisplayNextSentence(dialogue); });
+
         animator.SetBool("IsOpen", true);
 
-        nameText.text = dialogue.name;
+        nameText.text = dialogue.characterName;
 
         sentences.Clear();
 
-        foreach(string sentence in dialogue.sentences)
+        foreach (string sentence in dialogue.sentences)
         {
             sentences.Enqueue(sentence);
         }
@@ -53,34 +58,35 @@ public class DialogueManager : MonoBehaviour {
                 //_responses.choiceText = choiceText;
                 Text displayChoiceText = responseObject.GetComponentInChildren<Text>();// = _responses.choiceText;
                 displayChoiceText.text = choiceText;
-                
-                
+
+
                 Dialogue _nextDialogue = null;
                 Dialogue nextDialogue = _responses.GetNextDialogue(_nextDialogue);
                 Button responseButton = responseObject.GetComponent<Button>();
+                responseButton.onClick.RemoveAllListeners();
                 responseButton.onClick.AddListener(() => { ResponseClicked(nextDialogue); });
             }
-                continueButton.SetActive(false);
-            
+            continueButton.SetActive(false);
+
             //EndDialogue();
         }
-        if (sentences.Count == 0)
+        if (sentences.Count == 0 && dialogue.responses.Length == 0)
         {
-           //foreach(Response _responses in dialogue.responses)
-           //{
-           //    Instantiate<GameObject>(responsePrefab as GameObject);
-           //    //_responses.choiceText = dialogue.cho
-           //}
+            //foreach(Response _responses in dialogue.responses)
+            //{
+            //    Instantiate<GameObject>(responsePrefab as GameObject);
+            //    //_responses.choiceText = dialogue.cho
+            //}
             EndDialogue();
             return;
         }
-        string sentence =  sentences.Dequeue();
+        string sentence = sentences.Dequeue();
         StopAllCoroutines();
         StartCoroutine(TypeSentence(sentence));
 
     }
 
-    IEnumerator TypeSentence (string sentence)
+    IEnumerator TypeSentence(string sentence)
     {
         dialogueText.text = "";
         foreach (char letter in sentence.ToCharArray())
@@ -93,17 +99,17 @@ public class DialogueManager : MonoBehaviour {
     public void EndDialogue()
     {
         animator.SetBool("IsOpen", false);
-        //Debug.Log("End Of Conversation");
+        Debug.Log("End Of Conversation");
     }
 
     public void ResponseClicked(Dialogue _nextDialogue)
     {
         continueButton.SetActive(true);
-        Debug.Log("now true");
         GameObject[] responseObjects = GameObject.FindGameObjectsWithTag("responseObject");
-        foreach(GameObject go in responseObjects)
-        { 
-             GameObject.Destroy(go);
+        foreach (GameObject go in responseObjects)
+        {
+            GameObject.Destroy(go);
+            Debug.Log("destoryed " + go);
         }
         StartDialogue(_nextDialogue);
     }
