3353cdfa532b3a62a5ee6f03d7d3d7bdecca3e6e
Alex D <30276530+Doomus@users.noreply.github.com>
2020-12-15 16:23:40 -0800

    Camera Switching
    added in new scripts to handle switching camera placement switching including to follow player

---
+++ Assets/Scripts/CameraShiftTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShiftTrigger : MonoBehaviour
+{
+    public Camera mainCamPosSwitch;
+    public GameObject transition_InPos;
+    public GameObject transition_OutPos;
+
+
+    private void Start()
+    {
+        if(mainCamPosSwitch == null)
+        {
+            mainCamPosSwitch = Camera.main;
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            mainCamPosSwitch.GetComponent<CameraShift>().obj_CamPosObject = transition_InPos;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            mainCamPosSwitch.GetComponent<CameraShift>().obj_CamPosObject = transition_OutPos;
+        }
+    }
+}
