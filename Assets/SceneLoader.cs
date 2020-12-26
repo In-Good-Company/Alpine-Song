4818c4700ad68cc3610311b86f0adf6d37be36e6
smithDamon <35517446+smithDamon@users.noreply.github.com>
2020-12-26 15:18:14 -0800

    Added Scene loader Script

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : Interactable
+{
+    // Start is called before the first frame update
+    public Animator transition;
+    public string sceneName;
+    public Scene sceneToLoad;
+    public float transitionTime = 1f;
+
+    public override void Interact()
+    {
+        base.Interact();
+        LoadNextScene();
+    }
+    //void Start()
+    //{
+    //    
+    //}
+    //
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    
+    //}
+
+    public void LoadNextScene()
+    {
+        StartCoroutine(LoadScene(sceneName));
+        //SceneManager.LoadScene(sceneToLoad.buildIndex);
+    }
+
+    IEnumerator LoadScene(string scene)
+    {
+        transition.SetTrigger("Start");
+        Debug.Log("Started Wait");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(scene);
+        
+        
+        //play animaton
+
+        //wait
+
+        //Load it
+    }
+}
